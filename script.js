969af5ac2973191cfc479dc6aeeb50657ef6cb36
Ranulfson <alexei.kurganov@ngs.ru>
2021-02-27 17:09:05 +0700

    update

--- script.js
+++ script.js
@@ -8,12 +8,34 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?'),
-      b = prompt('На сколько оцените его?'),
-      c = prompt('Один из последних просмотренных фильмов?'),
-      d = prompt('На сколько оцените его?');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+let i = 0;
+while (i < 2) {
+    const a = prompt('Один из последних просмотренных фильмов?'),
+    b = prompt('На сколько оцените его?');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        confirm('Введите текст');
+        i--;
+    }
+    i++;
+}
+
+if (personalMovieDB.count < 10) {
+    confirm('Посмотрено довольно мало фильмом');
+    console.log('Посмотрено довольно мало фильмом');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    confirm('Вы классический зритель');
+    console.log('Вы классический зритель');
+} else if (personalMovieDB >= 30) {
+    confirm('Вы киноман');
+    console.log('Вы киноман');
+} else {
+    confirm('Error');
+    console.log('Error');
+}
 
 console.log(personalMovieDB);
